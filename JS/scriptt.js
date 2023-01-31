aade62b489dc67afdf2071fca173a9c69b74961d
Júlia de Lima <112833977+majulial@users.noreply.github.com>
2023-01-31 11:20:50 -0300

    Add files via upload

---
+++ JS/scriptt.js
@@ -0,0 +1,13 @@
+
+searchToggle = document.querySelector(".searchToggle");
+
+
+
+
+// js code to toggle search mode
+searchToggle.addEventListener("click", () => {
+   searchToggle.classList.toggle("active")
+
+});
+
+
